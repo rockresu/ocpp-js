4299939aa4c55832c1ad34812eac0e4af73b3b17
rockresu <schuepbs@gmail.com>
2019-05-24 10:45:10 +0200

    Added observers to as callbacks to the events

--- handlers/meterValues.js
+++ handlers/meterValues.js
@@ -2,9 +2,14 @@ const Promise = require('promise');
 const async = require('async');
 const DB = require('../db/index.js');
 var Storage = new DB(process.env.storage);
+var Subject = require('rxjs/index').Subject;
+
+var subject = new Subject()
+var observerable = subject.asObservable()
 
 module.exports = {
     handle: function(data) {
+        subject.next(data)
 
         return new Promise(function(resolve, reject) {
             var parsed = JSON.parse(JSON.stringify(data), function(k, v) {
@@ -17,13 +22,12 @@ module.exports = {
 
             // TODO: should filter by measurenad type [Issue #3]
             // TODO: find how to calculate station measurment [Issue #12]
-            // TODO: Get Unit [Issue #13]
-
-
+            // TODO: Get Unit [Issue #13
             // Store in Collection MeterValues
             resolve({
                 MeterValuesResponse: {}
             });
         });
-    }
+    },
+    observerable : observerable
 }
--- handlers/stopTransaction.js
+++ handlers/stopTransaction.js
@@ -3,8 +3,13 @@ const moment = require('moment');
 const Utils = require('../utils/utils.js');
 const DB = require('../db/index.js');
 var Storage = new DB(process.env.storage);
+var Subject = require('rxjs/index').Subject;
+
+var subject = new Subject()
+var observerable = subject.asObservable()
 
 module.exports = {
+    observerable : observerable,
     handle: function(data) {
         return new Promise(function(resolve, reject) {
             Storage.findById('transaction', data.transactionId, function(err, transactions) {
