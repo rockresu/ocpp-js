03d76c364bca62ed9e3d2822125df4c5283c9a50
Unknown <samuelsch@gmx.ch>
2019-06-06 09:43:39 +0200

    Surrounded functions with try catch
    They filedb functions seem to be not properly implemented. Surrounded them with nasty try catch...

--- db/file.js
+++ db/file.js
@@ -4,54 +4,66 @@ const fs = require('fs');
 const path = require('path');
 var TMP_DIR = '../tmp';
 
-let instance =  null;
+let instance = null;
 class File {
-  constructor() {
+    constructor() {
+    }
+
+    findAll(collection) {
+        try {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                return self.firebase.database().ref('/' + collection).once('value').then(function (snapshot) {
+                    var data = snapshot.val();
+                    resolve(data);
+                }).catch(function (error) {
+                    reject(error);
+                });
+            });
+        } catch(e){
+            pass
+        }        
   }
 
-  findAll(collection){
-    var self = this;
-    return new Promise(function(resolve, reject) {
-      return self.firebase.database().ref('/' + collection).once('value').then(function(snapshot){
-        var data = snapshot.val();
-        resolve(data);
-      }).catch(function(error){
-        reject(error);
-      });
-    });
-  }
+    findById(collection, id) {
+        var self = this;
+        return new Promise(function (resolve, reject) {
+            self.firebase.database().ref('/' + collection + '/' + id).once('value').then(function (snapshot) {
+                var data = snapshot.val();
+                resolve(data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    }
 
-  findById(collection, id){
-    var self = this;
-    return new Promise(function(resolve, reject) {
-      self.firebase.database().ref('/' + collection + '/' + id).once('value').then(function(snapshot){
-        var data = snapshot.val();
-        resolve(data);
-      }).catch(function(error){
-        reject(error);
-      });
-    });
-  }
+    save(collection, data) {
+        try {
+            var self = this;
 
-  save(collection, data){
-    var self = this;
+            var file = path.join(__dirname, TMP_DIR, '/' + collection + '.json');
+            return new Promise(function (resolve, reject) {
+                fs.writeFileSync(file, JSON.stringify(data, null, 4));
+                resolve();
+            });
+        }
+        catch (e) {
 
-    var file = path.join(__dirname, TMP_DIR, '/' + collection +'.json');
-    return new Promise(function(resolve, reject) {
-      fs.writeFileSync(file, JSON.stringify(data, null, 4));
-      resolve();
-    });
-  }
+        }
+    }
 
-  saveWithId(collection, id, data){
-    var self = this;
-    return new Promise(function(resolve, reject) {
-      self.firebase.database().ref('/' + collection + '/' + id).set(data).then(function(){
-        resolve({});
-      });
-    });
-  }
+    saveWithId(collection, id, data) {
+        try{
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                self.firebase.database().ref('/' + collection + '/' + id).set(data).then(function () {
+                    resolve({});
+                });
+            });
+        } catch(e){
 
+        }
+    }
 }
 
 module.exports = File;
